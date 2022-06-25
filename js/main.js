936a6fad0b50e776634d3f23a91721fd56c12d30
SL1ppeR <slippersasha@gmail.com>
2022-06-25 22:34:09 +0300

    end

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper('.swiper', {
+	speed: 800,
+	spaceBetween: 50,
+	navigation: {
+		nextEl: '.swiper-button-right',
+		prevEl: '.swiper-button-left'
+	},
+	initialSlide: 1,
+	loop: true,
+
+});
